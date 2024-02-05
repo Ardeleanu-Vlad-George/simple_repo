90fba1d8f5efc6d5cb7d9d987099f9d65f272e5f
Ardeleanu-Vlad-George <ardeleanu_vlad@ymail.com>
2024-02-05 13:04:33 +0200

    Added the option for plural

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 //Simple closure + github push test
-function newCounter(what){
+function newCounter(what, plural){
     let it = 0;
     function counter(){
-        console.log(it++ + ' ' + what);
+        console.log(it++ + ' ' + what + (plural ? '(s)' : ''));
     }
     return counter;
 }
 
 
-const cats = newCounter('cats');
+const cats = newCounter('cat', true);
 
 
 cats(); cats();
